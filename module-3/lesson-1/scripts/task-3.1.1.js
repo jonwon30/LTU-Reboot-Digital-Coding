ae72a5ad7c8617caaeb135f7eea1562eccc22f6e
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-01-25 11:05:56 +0000

    Update task-3.1.1.js

--- module-3/lesson-1/scripts/task-3.1.1.js
+++ module-3/lesson-1/scripts/task-3.1.1.js
@@ -1,2 +1,19 @@
-alert("Hello World");
-console.log("alert from the console");
+/* alert("Hello World");
+console.log("alert from the console"); */
+
+// alert("2nd alert message")
+console.log("2nd console log message");
+document.write("document write message")
+
+var messageTask = "New Message"
+console.log(messageTask);
+document.write(messageTask);
+
+var x = 10;
+var y = 20;
+
+console.log(x + y);
+console.log(x * y);
+console.log(x - y);
+console.log(x / y);
+console.log(x % y);
