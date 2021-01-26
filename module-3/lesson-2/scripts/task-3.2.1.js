ec7da44a7efcdcc0d79efc95b3fdc9c8065de791
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-01-26 10:00:44 +0000

    Update task-3.2.1.js
    add function message

---
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -0,0 +1,6 @@
+let functionNew = function () {
+    console.log("This is a function")
+    document.write("this is also part of the function")
+    console.log("Also part of the function" + " " + 5 * 10);
+}
+functionNew();
