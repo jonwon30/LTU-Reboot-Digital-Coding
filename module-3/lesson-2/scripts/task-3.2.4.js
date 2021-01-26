83decaab3281cbfec8359fea29c5a66717ffd556
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-01-26 11:31:20 +0000

    Update task-3.2.4.js
    add if else statement

---
+++ module-3/lesson-2/scripts/task-3.2.4.js
@@ -0,0 +1,14 @@
+let temp1 = prompt("what is the temperature between 1 and 100?")
+let tempnNum = parseInt(temp1)
+if (tempnNum <= 10 ){
+    console.log ("you need to wear a coat");
+} else if  (tempnNum > 10 && tempnNum < 20 ) {
+    console.log ("you may not need a coat");
+} else if (tempnNum >=20 && tempnNum < 42 ) {
+    console.log ("you won't need a coat");
+} else if (tempnNum >=42 && tempnNum <= 100 ) {
+    console.log ("wow thats hot!");
+} else {
+    console.log("please enter a number, that entry wasn't valid")
+};
+
