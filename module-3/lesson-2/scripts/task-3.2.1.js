6d66ff16b57e801e189167ddd23be9209998fe8d
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-01-26 10:33:21 +0000

    Update task-3.2.1.js
    add function with var

--- module-3/lesson-2/scripts/task-3.2.1.js
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -4,3 +4,19 @@ let functionNew = function () {
     console.log("Also part of the function" + " " + 5 * 10);
 }
 functionNew();
+
+
+function test (a, b) { // a and b can be called anything.
+    console.log(a + b); // using the placeholders in the brackers above.
+}
+
+test(10, 15) // a = 10, b = 15.  test calls the function and the numbers represent a, b.
+
+function fullName (firstName, LastName) {
+    fullName = firstName + " " + LastName
+    console.log("Your full name is " + fullName)
+}
+
+let firstName = prompt("What is your first Name?");
+let lastName = prompt("What is your last Name?");
+fullName(firstName, lastName)
