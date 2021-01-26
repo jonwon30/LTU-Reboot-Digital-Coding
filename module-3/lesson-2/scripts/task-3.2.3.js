426db87ab61b5423620fa550932b1d4db4e52552
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-01-26 10:50:23 +0000

    Update task-3.2.3.js
    Add show fullName function and return value.

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -10,5 +10,16 @@ var message = greet('Hello', 'Luke');
 /* EXAMPLE */
 
 // ADD YOUR CODE BELOW HERE
+let firstName = prompt("What is your first Name?");
+let lastName = prompt("What is your last Name?");
+
+function fullName (firstName, LastName) {
+  fullName = firstName + " " + LastName
+  return("Your full name is " + fullName)
+};
+
+var finalmessage = fullName(firstName, lastName);
+console.log(finalmessage);
+
 
 // ADD YOUR CODE ABOVE HERE
