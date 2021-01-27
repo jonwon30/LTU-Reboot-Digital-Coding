65dc5cfbdb90e04a1a60f2b2e973c374d2c310b3
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-01-27 10:19:05 +0000

    Update task-3.3.1.js
    add percent calculator

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,10 @@
+let num1 = 100;
+let num2 = 10;
+
+function numPercent (a, b) {
+    let percentage = a * b / 100;
+    total = num1 + percentage
+    return total;
+}
+
+console.log(numPercent(num1, num2));
