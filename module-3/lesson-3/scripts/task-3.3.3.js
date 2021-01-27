88a3fab0c955bedd4b07facf3af4959a669d92cc
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-01-27 11:11:07 +0000

    Update task-3.3.3.js
    Add javascript basic calculator

---
+++ module-3/lesson-3/scripts/task-3.3.3.js
@@ -0,0 +1,29 @@
+function calculator (num1, operator, num2,) {
+
+    switch (operator) {
+    case '+':
+        return num1 + num2;
+        break
+    case '-':
+        return num1 - num2;
+        break       
+    case '/':
+        return num1 / num2;
+        break
+    case '*':
+        return num1 * num2;
+        break
+    case '%':
+        return num1 % num2;
+        break
+    default:
+        return "not possible"
+}
+}
+
+let number1 = 16;
+let number2 = 7;
+let operator = '+';
+
+let totalNum = calculator(number1,operator,number2)
+console.log (`number ${number1} ${operator} ${number2} totals ${totalNum}`)
