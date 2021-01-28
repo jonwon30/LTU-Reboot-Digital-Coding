c88e37376b2c8e1174b284e8f3d69b8542666969
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-01-28 10:12:47 +0000

    Create task-3.4.1.js
    Add times table for loop

---
+++ module-3/lesson-4/scripts/task-3.4.1.js
@@ -0,0 +1,7 @@
+let message = alert("This is a times table programme");
+let timesTable = prompt("Enter for the times table");
+let timesTable1 = parseInt(timesTable);
+
+for(i = 1; i <= 12; i++) {
+    console.log(`${timesTable} * ${i} = ${timesTable1 * i}`)
+}
