94af5a286f090da57fcf23af1ebd0931b749c707
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-02-01 09:22:30 +0000

    Update task-3.4.4.js

--- module-3/lesson-4/scripts/task-3.4.4.js
+++ module-3/lesson-4/scripts/task-3.4.4.js
@@ -3,16 +3,9 @@ let favouriteRecipe = {
     servings: 2,
     ingredients: ["bun","meat","onion","ketchup"],
     directions: ["cook the meat","add meat to the buns", "top off with onion and ketchup"],
-
+    letsCook: function () {
+       console.log (`I'm hungry lets cook ${favouriteRecipe.recipeTitle}`)
+    },
 }
 
-// let favIngredients = favouriteRecipe.ingredients;
-// console.log(favouriteRecipe.ingredients)
-
-let ingredients = function() {
-for (i = 0; i < favIngredients.length; i++){
-    return(favIngredients[i])
-}
-}
-
-ingredients()
+console.log(favouriteRecipe.letsCook);
