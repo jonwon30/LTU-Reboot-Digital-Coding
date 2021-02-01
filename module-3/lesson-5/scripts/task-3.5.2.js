35ad1d926a7fb7dd98930eca396002231a13ff19
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-02-01 11:22:03 +0000

    Create task-3.5.2.js

---
+++ module-3/lesson-5/scripts/task-3.5.2.js
@@ -0,0 +1,31 @@
+/*
+let finalPrice = 0
+let discountPrice = 0
+let dicountPercentage = 20
+
+function foodShopDiscount (cart) {
+    for (i = 0; i < cart.length; i++) {
+            discountPrice += cart[i].price * cart[i].quantity
+            finalPrice = discountPrice * dicountPercentage / 100
+            return discountPrice - finalPrice.toFixed(2)
+            } else {
+                totalPrice2 += cart[i].price * cart[i].quantity
+            } 
+        }
+    }
+    */
+totalPrice1 = 0
+
+    function foodShopDiscount (cart) {
+        for (i = 0; i < cart.length; i++) {
+            if (cart[i].type === "food") {
+                let discount = (cart[i].price * 20 / 100 );
+                totalPrice1 += (cart[i].price) - discount * cart[i].quantity
+            } else {
+                totalPrice1 += totalPrice1 += cart[i].price * cart[i].quantity
+            }
+            }
+            return totalPrice1
+        }
+
+console.log(foodShopDiscount(shoppingCart))
