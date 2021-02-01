c3b18c401fdfceffc50831151e447254c92cc674
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-02-01 09:39:50 +0000

    Update task-3.4.4.js
    add method

--- module-3/lesson-4/scripts/task-3.4.4.js
+++ module-3/lesson-4/scripts/task-3.4.4.js
@@ -3,9 +3,10 @@ let favouriteRecipe = {
     servings: 2,
     ingredients: ["bun","meat","onion","ketchup"],
     directions: ["cook the meat","add meat to the buns", "top off with onion and ketchup"],
-    letsCook: function () {
-       console.log (`I'm hungry lets cook ${favouriteRecipe.recipeTitle}`)
+    letsCook: function (food) {
+       console.log ("I'm hungry lets cook" + food)
     },
 }
 
-console.log(favouriteRecipe.letsCook);
+console.log(favouriteRecipe.letsCook("pizza"));
+console.log("hello World")
