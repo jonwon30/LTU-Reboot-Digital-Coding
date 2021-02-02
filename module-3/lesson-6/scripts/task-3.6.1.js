ee5bfbb537d7359c802e5c27d5a5c99823a7dd9f
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-02-02 10:53:18 +0000

    Create task-3.6.1.js
    add DOM modifications for img and p2

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,8 @@
+let logo = document.querySelector('img');
+    logo.setAttribute('src', 'https://i1.wp.com/tech-ish.com/wp-content/uploads/2014/10/Google-Logo.jpg?fit=1575%2C1575&ssl=1');
+
+let secondParagraph = document.querySelectorAll('p')[1];
+console.log(secondParagraph)
+secondParagraph.style.border = '4px solid green'
+secondParagraph.style.marginTop = '50px'
+secondParagraph.style.padding = '50px'
