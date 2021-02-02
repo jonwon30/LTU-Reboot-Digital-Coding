15c7d7ccbc1049b3938484bed9a19d18e9e7a66e
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-02-02 11:36:55 +0000

    Create task-3.6.3.js
    Add eventlistener

---
+++ module-3/lesson-6/scripts/task-3.6.3.js
@@ -0,0 +1,11 @@
+let button = document.querySelector('button');
+
+    //button.addEventListener( 'click', function () {
+        //alert("Boooom")
+    //}
+
+let message = function () {
+    alert("bigger Boooooooooooom")
+}
+
+button.addEventListener('click', message)
