30f89e4beda119f9db8e0892ff6c67cfc7342264
John Stapylton <71082969+jonwon30@users.noreply.github.com>
2021-02-04 11:36:11 +0000

    Update task-3.7.1.js
    add js for toggle images

--- module-3/lesson-7/scripts/task-3.7.1.js
+++ module-3/lesson-7/scripts/task-3.7.1.js
@@ -1,8 +1,12 @@
 const buttons = document.querySelectorAll('button.selection');
+
 const allimages = document.querySelectorAll('main .image-selections .images');
 
+const filterSearch = document.querySelector('input[name="filter"]')
 
-function toggleimages( toggleImagesFunction ) {
+const filterButton = document.querySelector('button.filter-button')
+
+function toggleImages( toggleImagesFunction ) {
   if( toggleImagesFunction === 'all' ) {
     allimages.forEach( function( image ) {
       image.style.display = 'flex';
@@ -15,11 +19,57 @@ function toggleimages( toggleImagesFunction ) {
         image.style.display = 'none';
       }
     });
-  }
-}
+  } // End of if else statement
+} // End of toggleimages function
+
+// toggleActiveClass
+function toggleActiveClass ( active ) {
+  buttons.forEach(function ( button ) {
+    button.classList.remove('active');
+  });
+  active.classList.add('active');
+} // End of toggleActiveClass
+
+
+// filter button
+
+filterButton.addEventListener('click', function(){
+
+  let inputValue = filterSearch.value;
+
+  let searchTerm =  inputValue.toLowerCase();
+
+    toggleImages ( searchTerm );
+
+
+      buttons.forEach( function ( button ){
+        let dataClass = button.getAttribute('data-class');
+
+          if( dataClass === searchTerm) {
+
+            button.classList.add( 'active');
+
+          } else {
+
+            button.classList.remove ('active');
+
+          } // End of if else statement
+
+          filterSearch.value = "";
+
+  }) // End of for each loop
+  
+}); // End of filter button event listener
+
 
 buttons.forEach( function( button ) {
+
   button.addEventListener('click', function() {
-    toggleimages( button.dataset.class );
+
+    let dataClass = button.dataset.class;
+    toggleImages( dataClass );
+
+    toggleActiveClass( button )
+
   });
 });
